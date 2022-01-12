7ee4b39b0638a7a0b1a637c2e2c1522feb0df4c4
Azim Ahmed <azim.ahmed7@gmail.com>
2022-01-11 23:06:01 -0500

    fix build issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,24 +6,26 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   const handleConnectTorus = useCallback(async () => {
-    const torus = new Torus();
-    await torus.init({
-      whiteLabel: {
-        theme: {
-          isDark: false,
-          colors: {
-            torusBrand1: "#282c34",
+    if (typeof window !== 'undefined') {
+      const torus = new Torus();
+      await torus.init({
+        whiteLabel: {
+          theme: {
+            isDark: false,
+            colors: {
+              torusBrand1: "#282c34",
+            },
           },
+          logoDark: "https://tkey.surge.sh/images/Device.svg", // Dark logo for light background
+          logoLight: "https://tkey.surge.sh/images/Device.svg", // Light logo for dark background
+          topupHide: false,
+          featuredBillboardHide: true,
+          disclaimerHide: true,
+          defaultLanguage: "en",
         },
-        logoDark: "https://tkey.surge.sh/images/Device.svg", // Dark logo for light background
-        logoLight: "https://tkey.surge.sh/images/Device.svg", // Light logo for dark background
-        topupHide: false,
-        featuredBillboardHide: true,
-        disclaimerHide: true,
-        defaultLanguage: "en",
-      },
-    });
-    await torus.login();
+      });
+      await torus.login();
+    }
   }, []);
 
   return (
